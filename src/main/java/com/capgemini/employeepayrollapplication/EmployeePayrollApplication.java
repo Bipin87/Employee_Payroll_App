af7b2e66ff97370630ed9cd39ca9d201d85646af
Bipin87 <sahubipin484@gmail.com>
2025-02-28 15:33:25 +0530

    [Bipin Kumar Sahu] Add: Employee Payroll logging levels, logging to console or file, logging patterns, etc added

--- src/main/java/com/capgemini/employeepayrollapplication/EmployeePayrollApplication.java
+++ src/main/java/com/capgemini/employeepayrollapplication/EmployeePayrollApplication.java
@@ -1,13 +1,18 @@
 package com.capgemini.employeepayrollapplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App Started in {} environment", context.getEnvironment().getProperty("environment"));
+
     }
 
 }
